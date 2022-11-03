d1d068a65a07462d68e945763bf1ffedee2bf293
Natália Serrão <nataliaserrao45@gmail.com>
2022-11-03 08:28:06 -0400

    feat: sincronizando dados com o banco

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,11 @@ import AppDataSource from "./data-source";
 import "dotenv/config";
 
 (async () => {
-  await AppDataSource.initialize().catch(err => {
+  await AppDataSource.initialize().catch((err) => {
     console.error("Error during Data Source initialization", err);
   });
 
-  app.listen(process.env.PORT, () => {
+  app.listen(process.env.PORT || 3000, () => {
     console.log("Servidor executando");
   });
 })();
