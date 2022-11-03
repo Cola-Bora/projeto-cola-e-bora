74eb9a4c50ade7e2d50486d27fcc49c78873c0fc
Ingra Dantona <ingradantona.arq@gmail.com>
2022-11-03 09:04:44 -0400

    feat: criação da rota e controller de eventos

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,14 @@ import checkErrorMiddleware from "./middlewares/checkError.middleware";
 import userRoutes from "./routes/user.routes";
 import loginRoutes from "./routes/login.routes";
 import ongsRoutes from "./routes/ong.routes";
+import eventsRoutes from "./routes/events.routes";
 
 const app = express();
 app.use(express.json());
 app.use("/users", userRoutes);
 app.use("/login", loginRoutes);
 app.use("/ongs", ongsRoutes);
+app.use("/events", eventsRoutes)
 
 app.use(checkErrorMiddleware);
 
