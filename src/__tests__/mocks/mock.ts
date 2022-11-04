b124e586473685b51771385b840636c62cb4587a
Natália Serrão <nataliaserrao45@gmail.com>
2022-11-04 13:44:11 -0400

    feat: configs iniciais rotas de ongsII

--- src/__tests__/mocks/mock.ts
+++ src/__tests__/mocks/mock.ts
@@ -1,15 +1,15 @@
 import { IUserRequest } from "../../interfaces/user";
 
-export const mockedUser : IUserRequest = {
-    name: "Mario",
-    birthDate: "1998/10/12",
-    email: "mario@mail.com",
-    password: "12345678"
-}
+export const mockedUser: IUserRequest = {
+  name: "Mario",
+  birthDate: "1998/10/12",
+  email: "mario@mail.com",
+  password: "12345678",
+};
 
-export const mockedUserAdim : IUserRequest = {
-    name: "Mario",
-    birthDate: "1998/10/12",
-    email: "mario@mail.com",
-    password: "12345678"
-}
+export const mockedUserAdim: IUserRequest = {
+  name: "Mario",
+  birthDate: "1998/10/12",
+  email: "mario@mail.com",
+  password: "12345678",
+};
