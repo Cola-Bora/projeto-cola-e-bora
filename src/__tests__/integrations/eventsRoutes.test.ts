06aef7470509975e755c75ffb308dc5859e9166f
paulamurta <paulamurta@hotmail.com>
2022-11-04 10:34:00 -0400

    feat: Teste da rota GET events/eventId

--- src/__tests__/integrations/eventsRoutes.test.ts
+++ src/__tests__/integrations/eventsRoutes.test.ts
@@ -1,15 +1,16 @@
-import { DataSource } from "typeorm";
-import AppDataSource from "../../data-source";
-import app from "../../app"
+import { DataSource } from 'typeorm';
+import AppDataSource from '../../data-source';
+import { request } from 'http';
+import app from '../../app';
 
-describe("/events", () => {
+describe('/events', () => {
   let connection: DataSource;
 
   beforeAll(async () => {
     await AppDataSource.initialize()
       .then((res) => (connection = res))
       .catch((err) =>
-        console.log("Error during Data Source initialization", err)
+        console.log('Error during Data Source initialization', err)
       );
   });
 
@@ -17,6 +18,18 @@ describe("/events", () => {
     await connection.destroy();
   });
 
-  test("POST /events/:eventId -> register user in an event", async () => {
+  test('GET /events/:eventId -> should be able to list all the information of an event', async () => {
+    const event = await request(app).get('/events');
+    const response = await request(app).get(`/events/${event.body[0].id}`);
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('id');
+    expect(response.body).toHaveProperty('name');
+    expect(response.body).toHaveProperty('date');
+    expect(response.body).toHaveProperty('description');
+    expect(response.body).toHaveProperty('address');
+    expect(response.body).toHaveProperty('ong');
+    expect(response.body).toHaveProperty('userEvents');
   });
 });
+
+
