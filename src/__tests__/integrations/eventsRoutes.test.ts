72b29382678135ba42af52e0450cc151ba4b1ae7
Hyaguinhogp <hyago.v.a.m@gmail.com>
2022-11-04 08:16:42 -0400

    feat: comecando testes

--- src/__tests__/integrations/eventsRoutes.test.ts
+++ src/__tests__/integrations/eventsRoutes.test.ts
@@ -1,6 +1,12 @@
 import { DataSource } from "typeorm";
 import AppDataSource from "../../data-source";
 import app from "../../app"
+import { Categories } from "../../entities/ongCategory";
+import { Ongs } from "../../entities/ong";
+import { Addresses } from "../../entities/adress";
+import { Events } from "../../entities/event";
+import { mockedAddress, mockedEvent, mockedOng, mockedOngCategory } from "../mocks/mock";
+import request from 'supertest';
 
 describe("/events", () => {
   let connection: DataSource;
@@ -15,6 +21,20 @@ describe("/events", () => {
 
   afterAll(async () => {
     await connection.destroy();
+  });
+
+  test("POST /events -> create event", async () => {
+
+    const categoryRepository = AppDataSource.getRepository(Categories);
+    const ongRepository = AppDataSource.getRepository(Ongs);
+    const addressRepository = AppDataSource.getRepository(Addresses);
+    const eventRepository = AppDataSource.getRepository(Events);
+
+    await categoryRepository.save(mockedOngCategory);
+    await ongRepository.save(mockedOng);
+    await addressRepository.save(mockedAddress);
+
+    const response = await request(app).post('/events').set('Authorization', `Bearer`)
   });
 
   test("POST /events/:eventId -> register user in an event", async () => {
--- src/__tests__/mocks/mock.ts
+++ src/__tests__/mocks/mock.ts
@@ -1,3 +1,4 @@
+import { IEventRequest } from "../../interfaces/event";
 import { IUserRequest } from "../../interfaces/user";
 
 export const mockedUser : IUserRequest = {
@@ -12,4 +13,35 @@ export const mockedUserAdim : IUserRequest = {
     birthDate: "1998/10/12",
     email: "mario@mail.com",
     password: "12345678"
+}
+
+export const mockedOngCategory = {
+    id: '1',
+    name: 'category'
+}
+
+export const mockedOng = {
+    id: '1',
+    name: 'ong',
+    email: 'ong@gmail.com',
+    tel: '9999999999',
+    description: 'description',
+    cnpj: '0000000000000',
+    categoryId: 1
+}
+
+export const mockedAddress = {
+    id: '1',
+    street: 'rua',
+    number: '880',
+    cep: '69400797',
+    extra: 'casa'
+}
+
+export const mockedEvent : IEventRequest = {
+    name: "Event",
+    description: "Description",
+    date: "2022/11/22",
+    addressId: '1',
+    ongId: '1'
 }
