dc0990c057b3e9a705cdcda2cdcefd0d2a532b47
unknown <elizeucosta21@gmail.com>
2022-11-05 09:40:49 -0300

    feat: adicionada rota users/payments

--- src/app.ts
+++ src/app.ts
@@ -5,10 +5,12 @@ import checkErrorMiddleware from "./middlewares/checkError.middleware";
 import userRoutes from "./routes/user.routes";
 import loginRoutes from "./routes/login.routes";
 import ongsRoutes from "./routes/ong.routes";
+import paymentsRoutes from "./routes/payment.routes";
 
 const app = express();
 app.use(express.json());
 app.use("/users", userRoutes);
+app.use("/users/payments", paymentsRoutes);
 app.use("/login", loginRoutes);
 app.use("/ongs", ongsRoutes);
 
