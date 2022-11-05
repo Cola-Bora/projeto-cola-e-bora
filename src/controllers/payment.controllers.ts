5d6a33aa98d40d56215962484093edfa0a787457
unknown <elizeucosta21@gmail.com>
2022-11-05 09:48:45 -0300

    feat: adicionado controllers da rota de pagamentos

---
+++ src/controllers/payment.controllers.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from "express";
+import { IPaymentRequest } from "../interfaces/payment";
+import createPaymentServices from "../services/payment/createPayment.services";
+import deletePaymentServices from "../services/payment/deletePayment.services";
+import updatePaymentServices from "../services/payment/updatePayment.services";
+
+async function createPaymentControllers(req: Request, res: Response) {
+  const id = req.params.id;
+  const payment: IPaymentRequest = req.body;
+  const newPayment = await createPaymentServices(payment, id);
+  return res.status(201).json({ message: newPayment });
+}
+
+async function updatePaymentControllers(req: Request, res: Response) {
+  const id = req.params.id;
+  const payment: IPaymentRequest = req.body;
+  const updatePayment = await updatePaymentServices(payment, id);
+  return res.status(200).json({ message: updatePayment });
+}
+
+async function deletePaymentControllers(req: Request, res: Response) {
+  const id = req.params.id;
+  const deletePayment = await deletePaymentServices(id);
+  return res.status(204).json({ message: deletePayment });
+}
+
+export {
+  createPaymentControllers,
+  updatePaymentControllers,
+  deletePaymentControllers,
+};
