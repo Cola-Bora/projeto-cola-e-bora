a056159a6b88476da24f984eccde9a2140a4bd10
unknown <elizeucosta21@gmail.com>
2022-11-08 11:18:40 -0300

    refactor: modificação do título dos testes para inglês

--- src/__tests__/integrations/category/categoryRoutes.test.ts
+++ src/__tests__/integrations/category/categoryRoutes.test.ts
@@ -1,33 +1,33 @@
-import request from 'supertest';
-import { DataSource } from 'typeorm';
-import app from '../../app';
-import AppDataSource from '../../data-source';
-import { mockedUser, mockedUserAdim, mockedUserLogin } from '../mocks/mock';
-import createBaseCategoriesService from '../../services/categories/createBaseCategories.service';
+import request from "supertest";
+import { DataSource } from "typeorm";
+import app from "../../../app";
+import AppDataSource from "../../../data-source";
+import { mockedUser, mockedUserAdim, mockedUserLogin } from "../../mocks/mock";
+import createBaseCategoriesService from "../../../services/categories/createBaseCategories.service";
 
-describe('/categories', () => {
+describe("/categories", () => {
   let connection: DataSource;
 
   beforeAll(async () => {
     await AppDataSource.initialize()
-      .then((res) => (connection = res))
-      .catch((err) =>
-        console.log('Error during Data Source initialization', err)
+      .then(res => (connection = res))
+      .catch(err =>
+        console.log("Error during Data Source initialization", err)
       );
 
-    await request(app).post('/users').send(mockedUser);
-    await request(app).post('/users').send(mockedUserAdim);
+    await request(app).post("/users").send(mockedUser);
+    await request(app).post("/users").send(mockedUserAdim);
   });
   afterAll(async () => {
     await connection.destroy();
   });
 
-  test('GET /categories -> should be able to list all the categories of an ong', async () => {
-    const user = await request(app).post('/login').send(mockedUserLogin);
+  test("GET /categories -> should be able to list all the categories of an ong", async () => {
+    const user = await request(app).post("/login").send(mockedUserLogin);
     await createBaseCategoriesService();
     const response = await request(app)
-      .get('/categories')
-      .set('Authorization', `Bearer ${user.body.token}`);
+      .get("/categories")
+      .set("Authorization", `Bearer ${user.body.token}`);
     expect(response.body).toHaveLength(9);
     expect(response.status).toBe(200);
   });
